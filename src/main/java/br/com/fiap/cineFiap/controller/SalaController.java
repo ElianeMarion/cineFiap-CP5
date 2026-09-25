c76c1079202272d6025169fe4f054738f5643400
BeaUrbano <beatrizurbanosilva@gmail.com>
2026-09-25 08:35:34 -0300

    feat: etapa 1 - listagem de todas as salas ativas (GET /salas)

--- src/main/java/br/com/fiap/cineFiap/controller/SalaController.java
+++ src/main/java/br/com/fiap/cineFiap/controller/SalaController.java
@@ -1,54 +1,23 @@
 package br.com.fiap.cineFiap.controller;
 
-import br.com.fiap.cineFiap.dao.SalaDAO;
 import br.com.fiap.cineFiap.models.Sala;
+import br.com.fiap.cineFiap.service.SalaService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Objects;
 
 @RestController
 @RequestMapping("/salas")
 public class SalaController {
-    private SalaDAO dao = new SalaDAO();
 
+    @Autowired
+    private SalaService service;
 
-    public void cadastrar( Sala sala){
-
-            dao.cadastrar(sala);
-
-    }
-
-
-    public Sala buscarPorId( Long id){
-        return  dao.buscarPorId(id);
-
-
-    }
-
-    public List<Sala> salasEmCartaz(){
-        return dao.listar();
-    }
-
-
-    public void excluir ( Long id){
-
-            dao.excluir(id);
-
-    }
-
-    public void alterar( Long id, Sala objeto){
-
-            dao.alterar(objeto);
-    }
-
-
-    public void deletar(@PathVariable Long id){
-
-            dao.deletar(id);
-
-
+    @GetMapping
+    public List<Sala> listar() {
+        return service.listar();
     }
 }
