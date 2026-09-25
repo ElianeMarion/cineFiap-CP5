ff54ef564e693a5131dfc5420978110a285d9d78
Tadeul <gabrielttb02@gmail.com>
2026-09-25 08:36:33 -0300

    feat: etapa 1 - listagem de todas as salas ativas (GET /salas)

--- src/main/java/br/com/fiap/cineFiap/controller/SalaController.java
+++ src/main/java/br/com/fiap/cineFiap/controller/SalaController.java
@@ -2,53 +2,42 @@ package br.com.fiap.cineFiap.controller;
 
 import br.com.fiap.cineFiap.dao.SalaDAO;
 import br.com.fiap.cineFiap.models.Sala;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Objects;
 
 @RestController
 @RequestMapping("/salas")
 public class SalaController {
+
     private SalaDAO dao = new SalaDAO();
 
-
-    public void cadastrar( Sala sala){
-
-            dao.cadastrar(sala);
-
+    public void cadastrar(Sala sala) {
+        dao.cadastrar(sala);
     }
 
-
-    public Sala buscarPorId( Long id){
-        return  dao.buscarPorId(id);
-
-
+    public Sala buscarPorId(Long id) {
+        return dao.buscarPorId(id);
     }
 
-    public List<Sala> salasEmCartaz(){
+    public List<Sala> salasEmCartaz() {
         return dao.listar();
     }
 
-
-    public void excluir ( Long id){
-
-            dao.excluir(id);
-
+    public void excluir(Long id) {
+        dao.excluir(id);
     }
 
-    public void alterar( Long id, Sala objeto){
-
-            dao.alterar(objeto);
+    public void alterar(Long id, Sala objeto) {
+        dao.alterar(objeto);
     }
 
+    public void deletar(@PathVariable Long id) {
+        dao.deletar(id);
+    }
 
-    public void deletar(@PathVariable Long id){
-
-            dao.deletar(id);
-
-
+    @GetMapping
+    public List<Sala> listar() {
+        return dao.listar();
     }
 }
---
+++ src/main/java/br/com/fiap/cineFiap/service/SalaService.java
@@ -0,0 +1,12 @@
+package br.com.fiap.cineFiap.service;
+
+import br.com.fiap.cineFiap.models.Sala;
+
+import java.util.List;
+
+public class SalaService {
+
+    public List<Sala> listar() {
+        return List.of();
+    }
+}
