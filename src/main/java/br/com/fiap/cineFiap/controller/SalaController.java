e3562d5073b348f581873e71ae0e8533be2a289f
matheusfa08 <littlepacoquinha@gmail.com>
2026-09-25 08:30:23 -0300

    feat: etapa 1 - listagem de todas as salas ativas (GET /salas)

--- src/main/java/br/com/fiap/cineFiap/controller/SalaController.java
+++ src/main/java/br/com/fiap/cineFiap/controller/SalaController.java
@@ -2,6 +2,7 @@ package br.com.fiap.cineFiap.controller;
 
 import br.com.fiap.cineFiap.dao.SalaDAO;
 import br.com.fiap.cineFiap.models.Sala;
+import br.com.fiap.cineFiap.service.SalaService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,43 +13,11 @@ import java.util.Objects;
 @RestController
 @RequestMapping("/salas")
 public class SalaController {
-    private SalaDAO dao = new SalaDAO();
+    private SalaService salaService = new SalaService();
 
-
-    public void cadastrar( Sala sala){
-
-            dao.cadastrar(sala);
-
+    @GetMapping
+    public ResponseEntity<List<Sala>> listar(){
+        return ResponseEntity.ok(salaService.listar());
     }
 
-
-    public Sala buscarPorId( Long id){
-        return  dao.buscarPorId(id);
-
-
-    }
-
-    public List<Sala> salasEmCartaz(){
-        return dao.listar();
-    }
-
-
-    public void excluir ( Long id){
-
-            dao.excluir(id);
-
-    }
-
-    public void alterar( Long id, Sala objeto){
-
-            dao.alterar(objeto);
-    }
-
-
-    public void deletar(@PathVariable Long id){
-
-            dao.deletar(id);
-
-
-    }
 }
---
+++ src/main/java/br/com/fiap/cineFiap/service/SalaService.java
@@ -0,0 +1,16 @@
+package br.com.fiap.cineFiap.service;
+
+import br.com.fiap.cineFiap.dao.SalaDAO;
+import br.com.fiap.cineFiap.models.Sala;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class SalaService {
+    private SalaDAO salaDAO = new SalaDAO();
+
+    public List<Sala> listar(){
+        return salaDAO.listar();
+    }
+}
